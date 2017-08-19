6fd0d03178a3267fe1f5648ca9069ba851f31264
Francois Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2017-08-19 20:57:18 +0200

    switch to java

---
+++ src/main/java/org/humeurvagabonde/playtime/PlaytimeApplication.java
@@ -0,0 +1,13 @@
+package org.humeurvagabonde.playtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaytimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaytimeApplication.class, args);
+    }
+
+}
